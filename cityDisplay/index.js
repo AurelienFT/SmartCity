c475ffc15a5d986a737192ed49ff8ddcf38facab
Aurelien Foucault <aurelien.foucault@epitech.eu>
2019-12-09 16:49:44 +0100

    Add pythonAPI

--- cityDisplay/index.js
+++ cityDisplay/index.js
@@ -8,12 +8,22 @@ ctx.canvas.height = 1000;
 
 ctx.fillStyle = "#303030";
 ctx.fillRect(0 * cellSize, 0 * cellSize, cellSize, cellSize);
-for (var i = 0; i < ways.nodes.length; i++) {
-    console.log(ways.nodes[i]);
-    for (var x = ways.nodes[i].start.x; x <= ways.nodes[i].end.x; x++) {
-        ctx.fillRect(x * cellSize, ways.nodes[i].end.y * cellSize, cellSize, cellSize);
-    }
-    for (var y = ways.nodes[i].start.y; y <= ways.nodes[i].end.y; y++) {
-        ctx.fillRect(ways.nodes[i].end.x * cellSize, y * cellSize, cellSize, cellSize);
-    }
-}
+
+console.log("test");
+var headers = new Headers({
+    "Access-Control-Allow-Origin": "*"
+})
+
+fetch('http://localhost:5000/graphs/roads', {method: "GET", headers: headers})
+    .then(response => response.json())
+    .then(data => {
+        for (var i = 0; i < data.roads.length; i++) {
+            console.log(data.roads[i]);
+            for (var x = data.roads[i].start.x; x <= data.roads[i].end.x; x++) {
+                ctx.fillRect(x * cellSize, data.roads[i].end.y * cellSize, cellSize, cellSize);
+            }
+            for (var y = data.roads[i].start.y; y <= data.roads[i].end.y; y++) {
+                ctx.fillRect(data.roads[i].end.x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    })
