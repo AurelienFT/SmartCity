28a86b1531e89701cbd5ed619592405327b4120f
Aurelien Foucault <aurelien.foucault@epitech.eu>
2019-12-11 17:02:50 +0100

    Add broker mqtt

--- cityServer/manageMQTT.py
+++ cityServer/manageMQTT.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python3
-
-import paho.mqtt.client as mqtt
-
-# This is the Subscriber
+import paho.mqtt.client as mqttLib
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("topic/ping")
 
 def on_message(client, userdata, msg):
-  if msg.payload.decode() == "Pong!":
-    print("Yes!")
-    client.disconnect()
+  if msg.payload.decode() == "Ping!":
+      client.publish("topic/ping", "Pong!")
     
-client = mqtt.Client()
+client = mqttLib.Client()
 client.connect("localhost",1883,60)
-
 client.on_connect = on_connect
 client.on_message = on_message
-
-client.loop_forever()
